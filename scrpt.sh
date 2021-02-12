b464670ceba9237d3ad084c02d8bb3ea07ee5b74
Sasha Gatsiha <micro1985@mail.ru>
2021-02-12 11:00:55 +0300

    edited

--- scrpt.sh
+++ scrpt.sh
@@ -12,7 +12,6 @@ sudo mysql -u root -e "CREATE DATABASE wp6_database;"
 sudo mysql -u root -e "CREATE USER 'wp6_user'@'localhost' IDENTIFIED BY '123456';"
 sudo mysql -u root -e "GRANT ALL PRIVILEGES ON wp6_database.* TO 'wp6_user'@'localhost';"
 sudo mysql -u root -e "FLUSH PRIVILEGES;"
-sudo mysql -u root -e "exit "
 
 sudo wget https://ru.wordpress.org/latest-ru_RU.zip
 sudo unzip latest-ru_RU.zip -d /var/www/
