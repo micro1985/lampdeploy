93c37aac3791e1c54c34518d5bead1ff97fc6ea5
Sasha Gatsiha <micro1985@mail.ru>
2021-02-18 16:57:19 +0300

    working on it

--- osselect_deploy/scrpt.sh
+++ osselect_deploy/scrpt.sh
@@ -2,38 +2,44 @@
 
 sudo yum update -y
 
-sudo yum install httpd -y
-#sudo apt install mariadb-server mariadb-client -y
-#sudo apt install php php-mysql libapache2-mod-php php-cli php-cgi php-gd -y
+sudo yum install httpd zip -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+
+sudo yum install mariadb-server -y
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+
+sudo yum install php php-pear php-gd php-mysql -y
 
 echo "Enter sitename:"
 read SITENAME
 
-sudo touch /etc/apache2/sites-available/$SITENAME.conf
-#echo "<VirtualHost *:80>" | sudo tee /etc/apache2/sites-available/$SITENAME.conf
-#echo "	ServerName $SITENAME" | sudo tee -a /etc/apache2/sites-available/$SITENAME.conf
-#echo "	ServerAdmin webmaster@host" | sudo tee -a /etc/apache2/sites-available/$SITENAME.conf
-#echo "	DocumentRoot /var/www/$SITENAME" | sudo tee -a /etc/apache2/sites-available/$SITENAME.conf
+sudo touch /etc/httpd/conf.d/$SITENAME.conf
+echo "<VirtualHost *:80>" | sudo tee /etc/httpd/conf.d/$SITENAME.conf
+echo "	ServerName $SITENAME" | sudo tee -a /etc/httpd/conf.d/$SITENAME.conf
+echo "	ServerAdmin webmaster@host" | sudo tee -a /etc/httpd/conf.d/$SITENAME.conf
+echo "	DocumentRoot /var/www/$SITENAME" | sudo tee -a /etc/httpd/conf.d/$SITENAME.conf
 #errlog=$SITENAME"_error"
-#echo "	ErrorLog \${APACHE_LOG_DIR}/$errlog.log" | sudo tee -a /etc/apache2/sites-available/$SITENAME.conf
+echo "	ErrorLog /var/www/$SITENAME/log/errors.log" | sudo tee -a /etc/httpd/conf.d/$SITENAME.conf
 #acclog=$SITENAME"_access"
-#echo "	CustomLog \${APACHE_LOG_DIR}/$acclog.log combined" | sudo tee -a /etc/apache2/sites-available/$SITENAME.conf
-#echo "</VirtualHost>" | sudo tee -a /etc/apache2/sites-available/$SITENAME.conf
+echo "	CustomLog /var/www/$SITENAME/log/access.log combined" | sudo tee -a /etc/httpd/conf.d/$SITENAME.conf
+echo "</VirtualHost>" | sudo tee -a /etc/httpd/conf.d/$SITENAME.conf
 
-#sudo mysql_secure_installation
+sudo mysql_secure_installation
 
-#sudo mysql -u root -e "CREATE DATABASE wp_database;"
-#sudo mysql -u root -e "CREATE USER 'wp_user'@'localhost' IDENTIFIED BY '123456';"
-#sudo mysql -u root -e "GRANT ALL PRIVILEGES ON wp_database.* TO 'wp_user'@'localhost';"
-#sudo mysql -u root -e "FLUSH PRIVILEGES;"
+sudo mysql -u root -e "CREATE DATABASE wp_database;"
+sudo mysql -u root -e "CREATE USER 'wp_user'@'localhost' IDENTIFIED BY '123456';"
+sudo mysql -u root -e "GRANT ALL PRIVILEGES ON wp_database.* TO 'wp_user'@'localhost';"
+sudo mysql -u root -e "FLUSH PRIVILEGES;"
 
-#sudo wget https://ru.wordpress.org/latest-ru_RU.zip
-#sudo unzip latest-ru_RU.zip -d /var/www/
-#sudo mv /var/www/wordpress/ /var/www/$SITENAME/
+sudo wget https://ru.wordpress.org/latest-ru_RU.zip
+sudo unzip latest-ru_RU.zip -d /var/www/
+sudo mv /var/www/wordpress/ /var/www/$SITENAME/
 
-#sudo chown -R www-data:www-data /var/www/$SITENAME/
+sudo chown -R apache:apache /var/www/$SITENAME/
 #sudo a2ensite $SITENAME.conf
-#sudo service apache2 restart
+sudo service httpd restart
 
 
 
