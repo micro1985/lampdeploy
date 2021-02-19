a71436338b81cb663f4e5b1b5e32b77ec4a6401d
user <user@centos7.localdomain>
2021-02-19 08:22:12 +0300

    php7 added

--- osselect_deploy/scrpt.sh
+++ osselect_deploy/scrpt.sh
@@ -10,7 +10,12 @@ sudo yum install mariadb-server -y
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
 
-sudo yum install php php-pear php-gd php-mysql -y
+#sudo yum install php php-pear php-gd php-mysql -y
+
+sudo yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+sudo yum-config-manager -y --enable remi-php72
+sudo yum update
+sudo yum install -y php73 php73-php-fpm php73-php-mysqlnd php73-php-opcache php73-php-xml php73-php-xmlrpc php73-php-gd php73-php-mbstring php73-php-json
 
 echo "Enter sitename:"
 read SITENAME
@@ -36,6 +41,9 @@ sudo mysql -u root -e "FLUSH PRIVILEGES;"
 sudo wget https://ru.wordpress.org/latest-ru_RU.zip
 sudo unzip latest-ru_RU.zip -d /var/www/
 sudo mv /var/www/wordpress/ /var/www/$SITENAME/
+
+sudo mkdir /var/www/$SITENAME/log
+sudoo touch /var/www/$SITENAME/log/errors.log
 
 sudo chown -R apache:apache /var/www/$SITENAME/
 #sudo a2ensite $SITENAME.conf
