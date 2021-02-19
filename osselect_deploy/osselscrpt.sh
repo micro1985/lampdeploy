e1a8cacf1f22051b572509698781d69a3b3ca2cc
Sasha Gatsiha <micro1985@mail.ru>
2021-02-19 13:42:41 +0300

    ostest

--- osselect_deploy/osselscrpt.sh
+++ osselect_deploy/osselscrpt.sh
@@ -1,6 +1,10 @@
 RELEASE=`cat /etc/*-release`
 
+osname=""
+
 for name in 'CENTOS' 'UBUNTU' 'OpenSuse' 'DEBIAN' 'RED HAT'
 do
-        echo "${RELEASE}" | grep -i -q "${name}" && echo "$name"
+        echo "${RELEASE}" | grep -i -q "${name}" && osname=`echo "$name"`
 done
+
+echo "${osname}"
