7f51123b962a9d8612755d9f2ff53072a539593e
user <user@centos7.localdomain>
2021-02-19 09:42:56 +0300

    already working

--- osselect_deploy/scrpt.sh
+++ osselect_deploy/scrpt.sh
@@ -6,19 +6,11 @@ sudo yum install httpd zip -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
 
-sudo yum install mariadb-server -y
-sudo systemctl start mariadb
-sudo systemctl enable mariadb
-
-#sudo yum install php php-pear php-gd php-mysql -y
-
 sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 sudo yum-config-manager -y --enable remi-php72
-sudo yum update
-sudo yum install php php-fpm php-gd php-mysql
-
-#sudo yum install -y php74 php74-php-fpm php74-php-mysqlnd php74-php-opcache php74-php-xml php74-php-xmlrpc php74-php-gd php74-php-mbstring php74-php-json
+sudo yum update -y
+sudo yum -y install php php-fpm php-gd php-mysql
 
 echo "Enter sitename:"
 read SITENAME
@@ -28,13 +20,22 @@ echo "<VirtualHost *:80>" | sudo tee /etc/httpd/conf.d/$SITENAME.conf
 echo "	ServerName $SITENAME" | sudo tee -a /etc/httpd/conf.d/$SITENAME.conf
 echo "	ServerAdmin webmaster@host" | sudo tee -a /etc/httpd/conf.d/$SITENAME.conf
 echo "	DocumentRoot /var/www/$SITENAME" | sudo tee -a /etc/httpd/conf.d/$SITENAME.conf
-#errlog=$SITENAME"_error"
 echo "	ErrorLog /var/www/$SITENAME/log/errors.log" | sudo tee -a /etc/httpd/conf.d/$SITENAME.conf
-#acclog=$SITENAME"_access"
 echo "	CustomLog /var/www/$SITENAME/log/access.log combined" | sudo tee -a /etc/httpd/conf.d/$SITENAME.conf
 echo "</VirtualHost>" | sudo tee -a /etc/httpd/conf.d/$SITENAME.conf
 
-sudo mysql_secure_installation
+sudo yum install mariadb-server -y
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+
+cat << EOF | sudo mysql_secure_installation
+
+n
+y
+y
+y
+y
+EOF
 
 sudo mysql -u root -e "CREATE DATABASE wp_database;"
 sudo mysql -u root -e "CREATE USER 'wp_user'@'localhost' IDENTIFIED BY '123456';"
@@ -45,13 +46,8 @@ sudo wget https://ru.wordpress.org/latest-ru_RU.zip
 sudo unzip latest-ru_RU.zip -d /var/www/
 sudo mv /var/www/wordpress/ /var/www/$SITENAME/
 
-sudo mkdir /var/www/$SITENAME/log
-sudoo touch /var/www/$SITENAME/log/errors.log
+#sudo mkdir /var/www/$SITENAME/log
+#sudo touch /var/www/$SITENAME/log/errors.log
 
 sudo chown -R apache:apache /var/www/$SITENAME/
-#sudo a2ensite $SITENAME.conf
 sudo service httpd restart
-
-
-
-
